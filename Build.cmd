8512e95fcdf2c0daf9c7e1c3dc7c93a7305e9af0
Lord-Giganticus <mrubins64@gmail.com>
2021-09-24 19:59:40 -0600

    Add support for Project Template

--- Build.cmd
+++ Build.cmd
@@ -8,9 +8,15 @@ SET LD="%CD%\Kamek\Kamek.exe"
 
 SET LD_FLAGS=-externals=symbols/%1.txt -output-kamek=CustomCode_%1.bin
 
-FOR %%f in ("source\*.cpp") do %CXX% %CXX_FLAGS% build/%%~nf.o %%f
+if "%2"=="PT" mv source\ExtendedActorFactory.cpp %CD%
+
+PowerShell Get-ChildItem -Path .\source -Filter *.cpp -Recurse -File -Name > cppfiles.txt
+
+for /F "tokens=*" %%A in (cppfiles.txt) do %CXX% %CXX_FLAGS% build/%%~nA.o source\%%A
 
 rm -f *.d
+
+rm cppfile.txt
 
 make > lines.txt
 
@@ -21,3 +27,5 @@ rm lines.txt
 %LD% %OFILES% %LD_FLAGS%
 
 rm -f build/*.o
+
+if "%2"=="PT" mv ExtendedActorFactory.cpp source
